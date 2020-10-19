f96e05af72c6e3cf5e7d8f346ca4b89dc294f91a
jacmac2812 <simonsen.jac@icloud.com>
2020-10-19 18:38:43 +0200

    added build

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-XXXX="Name or your frontend project, for example movie --> folder you created under /var/www"
-DROPLET_URL="URL for your droplet"
+XXXX="ca2"
+DROPLET_URL="68.183.74.242"
 
 echo "##############################"
 echo "Building the frontend project"
