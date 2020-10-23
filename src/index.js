03681f1d86001645793341d5c1b6344238aa5184
jacmac2812 <simonsen.jac@icloud.com>
2020-10-23 12:28:36 +0200

    added links to sites and changed nav bar

--- src/index.js
+++ src/index.js
@@ -25,25 +25,23 @@ the Period2-week2-day3 Exercises
 */
 
 function hideAllShowOne(idToShow) {
-  document.getElementById("about_html").style = "display:none"
-  document.getElementById("ex1_html").style = "display:none"
-  document.getElementById("ex2_html").style = "display:none"
-  document.getElementById("ex3_html").style = "display:none"
+  document.getElementById("home_html").style = "display:none"
+  document.getElementById("api_html").style = "display:none"
+  document.getElementById("links_html").style = "display:none"
   document.getElementById(idToShow).style = "display:block"
 }
 
 function menuItemClicked(evt) {
   const id = evt.target.id;
   switch (id) {
-    case "ex1": hideAllShowOne("ex1_html"); break
-    case "ex2": hideAllShowOne("ex2_html"); break
-    case "ex3": hideAllShowOne("ex3_html"); break
-    default: hideAllShowOne("about_html"); break
+    case "api": hideAllShowOne("api_html"); break
+    case "links": hideAllShowOne("links_html"); break
+    default: hideAllShowOne("home_html"); break
   }
   evt.preventDefault();
 }
 document.getElementById("menu").onclick = menuItemClicked;
-hideAllShowOne("about_html");
+hideAllShowOne("home_html");
 
 
 
