d357d3e2c619fb7ccef237c79d601fc3d7acb79e
jacmac2812 <simonsen.jac@icloud.com>
2020-10-24 17:59:48 +0200

    added persons, list of persons and sort persons by phonenumber

--- src/index.js
+++ src/index.js
@@ -1,56 +1,12 @@
 import "./style.css"
 import "bootstrap"
 import "bootstrap/dist/css/bootstrap.css"
-import "./jokeFacade"
 import personFacade from "./personFacade"
-import jokeFacade from "./jokeFacade"
-
-
- 
-document.getElementById("all-content").style.display = "block"
-
-/*
-
-const URL ="https://jssimsen.com/tomcat/ca2/api/persons/all"
-
-
-personFacade.getAllUsers()
-.then(data=> {
-  const persons = data.all;
-  const tableRows = persons.map(person => `
-   <tr>
-    <td>${person.id}</td>
-    <td>${person.firstName}</td>
-    <td>${person.lastName}</td>
-    <td>${person.phone}</td>
-    <td>${person.street}</td>
-    <td>${person.zip}</td>
-    <td>${person.city}</td>
-    </tr>
-    `) 
-  const tableRowsAsStr = tablerows.join("");
-  document.getElementById("tbody").innerHTML = tableRowsAsStr
-
-})
-.catch(err => {
-  if (err.status) {
-    err.fullError.then(e =>  {
-      console.log(e.message)
-      document.getElementById("error").innerHTML = e.message;
-
-    })
-  }
-  else { console.log("Network error"); }
-});
-
-*/
-
-
 
 /* 
   Add your JavaScript for all exercises Below or in separate js-files, which you must the import above
 */
-
+document.getElementById("error").style.display = "none"
 /* JS For Exercise-1 below */
 
 
@@ -59,6 +15,126 @@ personFacade.getAllUsers()
 
 
 /* JS For Exercise-3 below */
+/*Her starter den function der laver en liste af alle personer*/
+document.getElementById("getAllBTN").addEventListener("click", makeListOfAllUsers);
+function makeListOfAllUsers() {
+  personFacade.getAllPersons()
+    .then(data => {
+      document.getElementById("error").style.display = "none"  /* søger for at fjerne error div'en når functionen bliver kaldt efter en fejl */
+      const persons = data.all;
+      const tableRows = personFacade.mapPerson(persons);
+      const tableRowsAsString = tableRows.join("");
+      document.getElementById("allUserRows").innerHTML = tableRowsAsString;
+    })
+    .catch(err => {
+      if (err.status) {
+        document.getElementById("error").style.display = "block"
+        err.fullError.then(e => {
+          document.getElementById("error").innerHTML = e.detail
+          console.log(e.detail)
+        })
+      }
+      else { console.log("Network error"); }
+    });
+}
+/*Her slutter den function der laver en liste af alle personer*/
+
+/*Her starter find person by phone number */
+document.getElementById("findByBTN").addEventListener("click", findByID);
+function findByID() {
+  let phone = document.getElementById("findByPhone").value;
+  personFacade.getById(phone)
+    .then(data => {
+      document.getElementById("error").style.display = "none" /* søger for at fjerne error div'en når functionen bliver kaldt efter en fejl */
+      const persons = [];
+      persons.push(data);
+      const tableRow = personFacade.mapPerson(persons);
+      const tableRowAsString = tableRow.join("");
+      document.getElementById("allUserRows").innerHTML = tableRowAsString;
+    })
+    .catch(err => {
+      if (err.status) {
+        document.getElementById("error").style.display = "block"
+        err.fullError.then(e => {
+          document.getElementById("error").innerHTML = e.detail
+          console.log(e.detail)
+        })
+      }
+      else { console.log("Network error"); }
+    });
+}
+/*Her slutter find person by phone number*/
+
+
+let addPersonBtn = document.getElementById("addPersonBtn");
+addPersonBtn.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  let saveBtn = document.getElementById("saveBtn");
+  saveBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let email = document.getElementById("email").value;
+    let street = document.getElementById("street").value;
+    let zipCode = document.getElementById("zipCode").value;
+    let number = document.getElementById("number").value;
+    let phoneDesc = document.getElementById("phoneDesc").value;
+
+    let name = document.getElementById("name").value;
+    let hobbyDesc = document.getElementById("hobbyDesc").value;
+    let city = document.getElementById("city").value;
+
+    const hobbyObject = {
+      name,
+      description : hobbyDesc
+    }
+
+    const phoneObject = {
+      number,
+      description : phoneDesc
+    }
+
+    const hobbies = [
+      hobbyObject
+    ]
+
+    const phones = [
+      phoneObject
+    ]
+
+
+
+    const newPerson = {
+      firstName,
+      lastName,
+      email,
+      street,
+      zipCode,
+      city,
+      phones,
+      hobbies
+    }
+
+
+    console.log(newPerson)
+    document.getElementById("error").innerHTML = "";
+
+    personFacade.addPerson(newPerson)
+      .then(makeListOfAllUsers())
+      .catch(err => {
+        if (err.status) {
+          err.fullError.then(e => document.getElementById("error").innerHTML = e.message)
+        } else { console.log("Network error"); }
+      });
+  });
+});
+
+
+let addPhoneCount = 0
+let editPhoneCount = 0
 
 
 /* 
@@ -67,24 +143,22 @@ the Period2-week2-day3 Exercises
 */
 
 function hideAllShowOne(idToShow) {
-  document.getElementById("home_html").style = "display:none"
-  document.getElementById("api_html").style = "display:none"
-  document.getElementById("links_html").style = "display:none"
+  document.getElementById("about_html").style = "display:none"
+  document.getElementById("ex1_html").style = "display:none"
+  document.getElementById("ex2_html").style = "display:none"
+  document.getElementById("ex3_html").style = "display:none"
   document.getElementById(idToShow).style = "display:block"
 }
 
 function menuItemClicked(evt) {
   const id = evt.target.id;
   switch (id) {
-    case "home": hideAllShowOne("home_html"); break
-    case "api": hideAllShowOne("api_html"); break
-    case "links": hideAllShowOne("links_html"); break
-    default: hideAllShowOne("home_html"); break
+    case "ex1": hideAllShowOne("ex1_html"); break
+    case "ex2": hideAllShowOne("ex2_html"); break
+    case "ex3": hideAllShowOne("ex3_html"); break
+    default: hideAllShowOne("about_html"); break
   }
   evt.preventDefault();
 }
 document.getElementById("menu").onclick = menuItemClicked;
-hideAllShowOne("home_html");
-
-
-
+hideAllShowOne("about_html");
--- src/personFacade.js
+++ src/personFacade.js
@@ -1,37 +1,72 @@
-"https://jssimsen.com/tomcat/ca2/api/persons/all"
-
-
-
-function hand1eHttpErrors(res) {
+const SERVER_URL = "https://jssimsen.com/tomcat/ca2/api/persons/"
+function handleHttpErrors(res) {
     if (!res.ok) {
-        return Promise.reject({status: res.status, fullError: res.json() })
+        return Promise.reject({ status: res.status, fullError: res.json() })
     }
     return res.json();
 }
 
 function makeOptions(method, body) {
     var opts = {
-        method: method, 
+        method: method,
         headers: {
             "Content-type": "application/json",
             "Accept": "application/json"
-
         }
     }
-    if(body){
+    if (body) {
         opts.body = JSON.stringify(body);
-
     }
     return opts;
 }
 
-function getAllUsers(){
-    fetch(URL)
-    .then(hand1eHttpErrors)
+function makeTable(){
+
+}
+function mapPerson(persons){
+   return persons.map(person => `
+    <tr>
+        <td>${person.id}</td>
+        <td>${person.firstName}</td>
+        <td>${person.lastName}</td>
+        <td>${person.email}</td>
+        <td>${person.street}</td>
+        <td>${person.zipCode}</td>
+        <td>${person.city}</td>
+        <td>${person.hobbies.map(hobby => hobby.name).join(",<br>")}</td>
+        <td>${person.phones.map(phone => phone.number).join(",<br>")}</td>
+    </tr>
+    `)
+}
+
+function getAllPersons() {
+    return fetch(SERVER_URL + "all")
+        .then(handleHttpErrors)
+}
+
+function getById(phone) {
+    return fetch(SERVER_URL + phone)
+        .then(handleHttpErrors)
+}
+
+function addPerson(person) {
+    const options = makeOptions("POST", person)
+    return fetch(SERVER_URL, options)
+        .then(handleHttpErrors)
+}
+
+function editPerson(person, id) {
+    const options = makeOptions("PUT", person)
+    return fetch(SERVER_URL + id , options)
+        .then(handleHttpErrors)
 }
 
 const personFacade = {
-    getAllUsers
+    getAllPersons,
+    getById,
+    mapPerson,
+    addPerson,
+    editPerson,
 }
 
 export default personFacade;
